17fc577faa52422d265f93295303aa2825d30785
João Félix <felixjoao651@gmail.com>
2025-04-29 14:56:41 -0400

    Create: pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// src/pages/HomePage.js
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Loja de Barbantes!</h1>
+      <p>Encontre os melhores barbantes para seus projetos de artesanato.</p>
+      {/* Adicione mais conteúdo da página inicial aqui */}
+    </div>
+  );
+};
+
+export default HomePage;
