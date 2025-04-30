9506e11ecccb2de66d17780f9ef492ce60c5f41e
João Félix <felixjoao651@gmail.com>
2025-04-30 15:45:13 -0400

    feat: Implement cart persistence using localStorage
    This commit introduces cart persistence to the application by utilizing the browser's `localStorage` API.
    
    - Modified the `useCart` custom Hook in `src/hooks/useCart.tsx` to load the cart items from `localStorage` upon initialization.
    - Added a `useEffect` hook that automatically saves the `cartItems` state to `localStorage` whenever it changes.
    - Defined a constant `CART_STORAGE_KEY` to manage the key used for storing cart data in `localStorage`.
    
    With this implementation, users' cart contents will now persist across browser sessions, improving the overall shopping experience.

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,6 +1,8 @@
 // src/hooks/useCart.tsx
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartItem, Product } from "../types/product";
+
+const CART_STORAGE_KEY = "catalogoBarbantesCart";
 
 interface UseCartResult {
   cartItems: CartItem[];
@@ -11,8 +13,14 @@ interface UseCartResult {
 }
 
 const useCart = (products: Product[]): UseCartResult => {
-  // <----- Verifique esta linha
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCart = localStorage.getItem(CART_STORAGE_KEY);
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cartItems));
+  }, [cartItems]); // O efeito será executado sempre que cartItems mudar
 
   const handleAddToCart = (productId: number) => {
     const existingItem = cartItems.find((item) => item.id === productId);
